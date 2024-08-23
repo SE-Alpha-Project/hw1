4a26b72b8b18b82a6a68923b732a1207f9853461
yash2705 <62518145+yash2705@users.noreply.github.com>
2024-08-23 18:22:10 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def fib(n):
+  if(n == 0): return 0
+  elif(n == 2 or n == 1): return 1
+  return fib(n-1) + fib(n-2)
+
+def main():
+    n = int(input("Which fibonacci number do you want to find? "))
+    print(fib(n))
+    
+main()
