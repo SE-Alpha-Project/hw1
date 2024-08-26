dddc2eee537238dee20270e3978c9afdf1d0557f
Ananya Patankar <ananya09patankar@gmail.com>
2024-08-26 14:46:39 -0400

    Update test_fibo.py

--- se_alpha_1/test_fibo.py
+++ se_alpha_1/test_fibo.py
@@ -3,5 +3,5 @@ import pytest
 from fibonacci import fib
 
 def test_fibo_recurs():
-    assert fib(4) == 3  # This should fail because the correct result is 3
+    assert fib(4) == 3  # This should pass because the correct result is 3
     assert fib(4) == 5  # This should fail because the correct result is 3
