a794ffe10416a2ce774b5dfc9ee06591986cbd01
Chaitralee Datar <chaitraleedatar41@gmail.com>
2024-08-26 14:45:17 -0400

    Update and rename test_fibo_positive.py to test_fibo.py

--- se_alpha_1/test_fibo.py
+++ se_alpha_1/test_fibo.py
@@ -2,8 +2,6 @@
 import pytest
 from fibonacci import fib
 
-def test_fibo_recurs_pass():
+def test_fibo_recurs():
     assert fib(4) == 3  # This should fail because the correct result is 3
-
-
-
+    assert fib(4) == 5  # This should fail because the correct result is 3
